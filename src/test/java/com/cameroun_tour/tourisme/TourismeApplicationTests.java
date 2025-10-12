e2ac552a83cb7980e3990ef315b2186c51252149
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2025-10-12 03:05:24 +0100

    Ajout des fichiers de configuration Maven et de l'application Spring Boot pour le projet de tourisme au Cameroun.

---
+++ src/test/java/com/cameroun_tour/tourisme/TourismeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cameroun_tour.tourisme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
