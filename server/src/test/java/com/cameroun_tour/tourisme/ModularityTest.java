5bce547e47d60c29ef71b9ecc3201e384a9b06f7
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2025-10-30 11:52:25 +0100

    feat: ajouter la configuration JWT et OAuth2 dans les fichiers de propriétés de test, et supprimer les tests inutilisés

--- server/src/test/java/com/cameroun_tour/tourisme/ModularityTest.java
+++ server/src/test/java/com/cameroun_tour/tourisme/ModularityTest.java
@@ -2,7 +2,9 @@ package com.cameroun_tour.tourisme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 public class ModularityTest {
 
     @Test
